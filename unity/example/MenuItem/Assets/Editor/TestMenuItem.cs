300b151f164340527d2e83aaef5afd225ae05b16
whisperlin <whisperlin@163.com>
2018-12-18 11:32:57 +0800

    菜单功能提交

---
+++ unity/example/MenuItem/Assets/Editor/TestMenuItem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class TestMenuItem : MonoBehaviour {
+
+	 
+
+	//& = Alt  #=shit  % = ctrl
+	[MenuItem("MenuItem/普通的顶部菜单  _%#c")]
+	private static void MenuItemFunc3()
+	{ 
+		Debug.Log("MenuItemFunc3");
+	}
+
+
+	[MenuItem("GameObject/在GameObject上",false,0)]
+	static bool menu1 () {
+		Object selectedObject = Selection.activeObject;
+		/*if(selectedObject != null && 
+			selectedObject.GetType() == typeof(GameObject))
+		{
+			return true;
+		}
+		return false;*/
+		return false;
+	}
+
+
+ 
+	 
+ 
+
+	
+	[MenuItem("Assets/在Project目录里右键1")]
+	private static void Assets_right_btn1()
+	{ 
+		Debug.Log("在Project目录里右键1");
+	}
+
+	[MenuItem("CONTEXT/Rigidbody/在Rigidbody上右键")]
+	private static void CONTEXT_Rigidbody_right_btn()
+	{ 
+		Debug.Log("在Rigidbody上右键");
+	}
+	[MenuItem("GameObject/UI/在GameObject目录里右键")]
+	private static void GameObject_right_btn()
+	{ 
+		Debug.Log("在GameObject目录里右键");
+	}
+}
---
+++ unity/example/MenuItem/Assets/TestMain.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+public class TestMain : MonoBehaviour
+{ 
+	 
+	[ContextMenu("ContextMenu1")] 
+	public void ContextMenuFunc1() 
+	{ 
+		Debug.Log("ContextMenu1");
+	} 
+	public int a = 0; 
+	public string b = ""; 
+	[ContextMenuItem("add testName", "ContextMenuFunc2")] 
+	public string testName = ""; 
+	private void ContextMenuFunc2() 
+	{ 
+		testName = "testName"; 
+	}
+}
