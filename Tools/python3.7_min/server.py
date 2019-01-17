20bcd92aaa706fdf2e90b79ae13e86cccf1fd3ad
whisperlin <whisperlin@163.com>
2019-01-17 11:24:36 +0800

    最小python服务器

---
+++ Tools/python3.7_min/server.py
@@ -0,0 +1,7 @@
+from http.server import HTTPServer, CGIHTTPRequestHandler  
+
+port = 8080  
+
+httpd = HTTPServer(('', port), CGIHTTPRequestHandler)  
+print("Starting simple_httpd on port: " + str(httpd.server_port))  
+httpd.serve_forever()     
---
+++ laya/LayaUnity/README.md
@@ -0,0 +1,3 @@
+﻿# Laya插件修改，添加材质，导出代码等。
+
+![image](https://github.com/whisperlin/utils/blob/master/laya/LayaUnity/demo.png) 
