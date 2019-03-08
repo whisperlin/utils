eae06c496f3c3935897acce019c1311091d89bd0
whisperlin <whisperlin@163.com>
2019-03-08 19:08:00 +0800

    基本光照模型

---
+++ unity/example/LightModel/Assets/LightInformation.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class LightInformation : MonoBehaviour {
+
+    public Light DirectionalLight;
+    public Light PointLight;
+    public Light Spotlight;
+
+    public bool updateInRuntime = false;
+
+    void UpdateLightData()
+    {
+        if (DirectionalLight)
+        {
+            Shader.SetGlobalColor("DirectionalLightColor", DirectionalLight.color);
+            Shader.SetGlobalVector("DirectionalLightDir", DirectionalLight.transform.forward);
+            Shader.SetGlobalFloat("DirectionalLightIntensity", DirectionalLight.intensity);
+
+
+            Shader.SetGlobalColor("PointLightColor", PointLight.color);
+            Shader.SetGlobalVector("PointLightPosition", PointLight.transform.position);
+            Shader.SetGlobalFloat("PointLightRange", PointLight.range);
+            Shader.SetGlobalFloat("PointLightIntensity", PointLight.intensity);
+
+
+
+
+            Shader.SetGlobalColor("SpotlightColor", Spotlight.color);
+            Shader.SetGlobalFloat("SpotlightSpotAngle0", Mathf.Cos(Spotlight.spotAngle* Mathf.Deg2Rad / 2.0f));
+            Shader.SetGlobalFloat("SpotlightSpotAngle1", Mathf.Cos(Spotlight.spotAngle * Mathf.Deg2Rad / 4.0f));
+            Shader.SetGlobalVector("SpotLightPosition", Spotlight.transform.position);
+            Shader.SetGlobalVector("SpotDirection", Spotlight.transform.forward);
+            Shader.SetGlobalFloat("SpotLightIntensity", Spotlight.intensity);
+            Shader.SetGlobalFloat("SpotLightRange", Spotlight.range);
+
+
+
+
+
+
+        }
+    }
+	// Use this for initialization
+	void Start () {
+        UpdateLightData();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(updateInRuntime)
+            UpdateLightData();
+    }
+}
