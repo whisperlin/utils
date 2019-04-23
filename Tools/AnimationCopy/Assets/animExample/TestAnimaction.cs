c494b21a01e3fc5ad6a28b47565f899ef763a47a
whisperlin <309762472@qq.com>
2019-04-23 18:33:23 +0800

    一个animation的例子也放这里

---
+++ Tools/AnimationCopy/Assets/animExample/TestAnimaction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnimaction : MonoBehaviour {
+
+    public void OnAnimFinish(string test)
+    {
+        Debug.Log("On Finish "+test);
+    }
+}
