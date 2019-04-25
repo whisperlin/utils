92de1c10df6f52a22fce85c0296d0877f5f88edd
whisperlin <309762472@qq.com>
2019-04-25 16:09:22 +0800

    提交json读取

---
+++ unity/example/TextAccess/Assets/ForText.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Json解析为该对象
+public class Response<T>
+{
+    public List<T> list;
+}
+
+[System.Serializable]
+public class Student
+{
+    public int id;
+    public string name;
+}
+public class ForText : MonoBehaviour {
+
+
+
+    // 解析Json的方法
+    public void ParseItemJson(string jsonStr)
+    {
+        // 将Json中的数组用一个list包裹起来，变成一个Wrapper对象
+        jsonStr = "{ \"list\": " + jsonStr + "}";
+        Response<Student> studentList = JsonUtility.FromJson<Response<Student>>(jsonStr);
+        foreach (Student item in studentList.list)
+        {
+            Debug.Log(item.id+" "+item.name);
+        }
+    }
+
+    
+    public TextAsset text;
+	void Start () {
+        ParseItemJson(text.text);
+        //Debug.Log(text.text);
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+}
