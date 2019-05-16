695ee9b538a0c8aaa114c6531ed25bb6d64d9de1
whisperlin <whisperlin@163.com>
2019-05-16 21:03:02 +0800

    T4M添加水画刷

---
+++ unity/T4MModify/Assets/SetDepthReplaymentShader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class SetDepthReplaymentShader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Camera>().SetReplacementShader(Shader.Find("TA/GetDepthFromOrthCamera"),null);
+	}
+}
---
+++ unity/T4MModify/Assets/TestImageEffect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestImageEffect : MonoBehaviour {
+
+    public Material _Material;
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (_Material)
+        {
+            Graphics.Blit(source, destination, _Material);
+        }
+    }
+
+ 
+}
