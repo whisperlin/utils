1a7da5d99479582e55aeeeec527c9530c70a47a0
whisperlin <309762472@qq.com>
2019-07-06 15:13:37 +0800

    uv test

---
+++ unity/example/UVTest/Assets/Editor/SaveMesh.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class SaveMesh : MonoBehaviour {
+	[MenuItem("TA/Create A Mesh")]
+	// Use this for initialization
+	static void Start () {
+		var mesh = new Mesh();
+		mesh.vertices = new Vector3[] { new Vector3(-1 ,-1,0), new Vector3(1, -1, 0), new Vector3(-1, 1, 0), new Vector3(1, 1,0) };
+		mesh.uv = new Vector2[] { new Vector2(0, 0), new Vector2(1, 0), new Vector2(0, 1), new Vector2(1, 1) };
+		mesh.triangles = new int[] { 0, 2, 1, 1, 2, 3 };
+		mesh.RecalculateNormals();
+
+		string path = EditorUtility.SaveFilePanelInProject ("save", "m", "asset", "save");
+		if (path.Length > 0) {
+			AssetDatabase.CreateAsset(mesh,path);
+		}
+	}
+	
+
+}
