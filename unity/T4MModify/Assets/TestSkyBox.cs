24f89345088cfc878f40d9e98421d52d8d4b9776
whisperlin <whisperlin@163.com>
2019-08-07 16:14:57 +0800

    地形例子

---
+++ unity/T4MModify/Assets/TestSkyBox.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class TestSkyBox : MonoBehaviour {
+
+    public Cubemap cube;
+    public Camera cam;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (null == cam)
+        {
+            cam = this.gameObject.AddComponent<Camera>();
+            
+        }
+
+        if (null != cube)
+            cam.RenderToCubemap(cube);
+
+
+    }
+}
