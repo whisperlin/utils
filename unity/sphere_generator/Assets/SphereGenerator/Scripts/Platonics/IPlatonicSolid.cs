1d03c4dee0b9eec81497bea695aa77dc70651d59
whisperlin <whisperlin@163.com>
2019-08-27 18:39:01 +0800

    Icosphere

---
+++ unity/sphere_generator/Assets/SphereGenerator/Scripts/Platonics/IPlatonicSolid.cs
@@ -0,0 +1,15 @@
+﻿// Original source: https://github.com/alexisgea/sphere_generator and post: https://www.alexisgiard.com/icosahedron-sphere-remastered/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlexisGea {
+	public interface IPlatonicSolid {
+			List<Vector3> Vertices { get; }
+			List<TriangleFace> Faces { get; }
+			Vector3 NorthPole { get; }
+
+			List<Vector3> RemapVertices(List<Vector3> vertices, List<TriangleFace> faces);
+	}
+}
